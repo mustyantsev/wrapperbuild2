5f610eff0d642e8b5578ac3c7ace35e60b6a5424
mustyantsev <mustyantsev@users.noreply.github.com>
2023-10-23 16:00:48 +0000

    Update to client-cpp v.2.0.0

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,1 @@
-from conans import ConanFile, CMake
 
-class clientCsharpConan(ConanFile):
-    generators = "cmake"
-    settings = "os", "compiler", "build_type", "arch"
-
-    def requirements(self):
-        self.requires("opentdf-client/1.5.0@")
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
